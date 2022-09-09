818df568878334c97b3c333a05ff0490d77dbaba
fabioscielzo <93041486+fabioscielzo@users.noreply.github.com>
2022-09-09 15:20:14 +0200

    n

--- myapp/app.py
+++ myapp/app.py
@@ -1,28 +1,17 @@
 from shiny import App, render, ui
 
-# Import modules for plot rendering
-import numpy as np
-import matplotlib.pyplot as plt
-
 app_ui = ui.page_fluid(
-    ui.layout_sidebar(
-        ui.panel_sidebar(
-            ui.input_slider("n", "N", 0, 100, 20),
-        ),
-        ui.panel_main(
-            ui.output_plot("plot"),
-        ),
-    ),
+    ui.h2("Hello Shiny!"),
+    ui.input_slider("n", "N", 0, 100, 20),
+    ui.output_text_verbatim("txt"),
 )
 
 
 def server(input, output, session):
     @output
-    @render.plot(alt="A histogram")
-    def plot():
-        np.random.seed(19680801)
-        x = 100 + 15 * np.random.randn(437)
-        plt.hist(x, input.n(), density=True)
+    @render.text
+    def txt():
+        return f"n*2 is {input.n() * 2}"
 
 
-app = App(app_ui, server, debug=True)
+app = App(app_ui, server)
