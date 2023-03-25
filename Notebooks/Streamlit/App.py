3aa04bc29ab6c3000050a3f985698874652d1d09
fabioscielzo <fabioscielzo98@gmail.com>
2023-03-25 22:25:21 +0100

    n

--- Notebooks/Streamlit/App.py
+++ Notebooks/Streamlit/App.py
@@ -1,9 +1,20 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
+st.title("Visualización de datos")
 
-button_clicked = st.button("Haz clic aquí")
-if button_clicked:
-    st.write("¡Has hecho clic en el botón!")
+# Cargar archivo CSV
+file = st.file_uploader("Cargar archivo CSV", type="csv")
+
+# Si se cargó un archivo, mostrar tabla de datos y gráfico
+if file is not None:
+    # Convertir archivo CSV en un DataFrame de Pandas
+    df = pd.read_csv(file)
+
+    # Mostrar tabla de datos
+    st.write("Tabla de datos:")
+    st.write(df)
 
 
 
